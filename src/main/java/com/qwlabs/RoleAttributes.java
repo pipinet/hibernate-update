8ad8010ca1b9329fe5dcd6686005dd96b8b2730e
Yoann Rodière <yoann@hibernate.org>
2024-02-05 13:00:42 +0100

    Demonstrate workaround

--- src/main/java/com/qwlabs/RoleAttributes.java
+++ src/main/java/com/qwlabs/RoleAttributes.java
@@ -1,5 +1,7 @@
 package com.qwlabs;
 
+import java.util.Objects;
+
 public class RoleAttributes {
     private String code;
 
@@ -9,5 +11,22 @@ public class RoleAttributes {
 
     public void setCode(String code) {
         this.code = code;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if ( this == o ) {
+            return true;
+        }
+        if ( o == null || getClass() != o.getClass() ) {
+            return false;
+        }
+        RoleAttributes that = (RoleAttributes) o;
+        return Objects.equals( code, that.code );
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash( code );
     }
 }
